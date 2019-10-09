5c77176061f2191591c51b2b556e84d2b968d832
lhx187 <2713697048@qq.com>
2019-10-09 10:20:29 +0800

    发现新大陆，暂停学习😂

--- lib/utils/android_back_desktop.dart
+++ lib/utils/android_back_desktop.dart
@@ -1,6 +1,25 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
+
 ///
 /// 安卓 - 返回手机桌面
 ///
-class AndroidBackDesktop{
+class AndroidBackDesktop {
+  static final String _channelName = "android.back.desktop";
+  static final String _backEventName = "backDesktop";
+  static final _platform = MethodChannel(_channelName);
 
+  static Future<bool> backDeskTop() async {
+    if (Platform.isFuchsia || Platform.isAndroid) {
+      try {
+        return !(await _platform.invokeMethod(_backEventName));
+      } on PlatformException catch (e) {
+        debugPrint("回到桌面失败：${e.toString()}");
+      }
+      return true;
+    }
+    return true;
+  }
 }
